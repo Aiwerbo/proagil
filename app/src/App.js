3e7285de7cad047f069ceafac88a8a928460e4e1
Andreas Iwerbo <andreas@iwerbo.se>
2019-12-16 20:09:19 +0100

    half way to starting point

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
+import Chess from 'chess.js'
+import 'react-chessground/dist/styles/chessground.css'
+var Chessground = require("chessground").Chessground;
+
 function App() {
+ const chess = new Chess()
+ console.log(chess)
+
+ useEffect(() => {
+   
+  Chessground(document.body, {});
+  
+ }, [])
+
+ while (!chess.game_over()) {
+  var moves = chess.moves();
+  var move = moves[Math.floor(Math.random() * moves.length)];
+  chess.move(move);
+}
+console.log(chess.pgn());
+
   return (
     <div className="App">
-    
-    
-
-
+ 
     </div>
   );
 }
