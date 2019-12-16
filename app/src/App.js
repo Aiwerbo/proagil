77d0111c65754f1c806b62c9fb90867d363ff18d
Andreas Iwerbo <andreas@iwerbo.se>
2019-12-16 22:37:30 +0100

    cleaned up the code in app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,20 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import Chess from 'chess.js';
+import 'react-chessground/dist/styles/chessground.css';
 
-import Chess from 'chess.js'
-import 'react-chessground/dist/styles/chessground.css'
-var Chessground = require("chessground").Chessground;
+const Chessground = require('chessground').Chessground;
 
 function App() {
- const chess = new Chess()
- console.log(chess)
+  const chess = new Chess();
+  console.log(chess);
 
- useEffect(() => {
-   
-  Chessground(document.body, {});
-  
- }, [])
-
- while (!chess.game_over()) {
-  var moves = chess.moves();
-  var move = moves[Math.floor(Math.random() * moves.length)];
-  chess.move(move);
-}
-console.log(chess.pgn());
+  useEffect(() => {
+    Chessground(document.body, {});
+  }, []);
 
   return (
     <div className="App">
- 
     </div>
   );
 }
