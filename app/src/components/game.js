5d9a059c7d9fd45805ae279863f9716cfbdad33d
Jim Nervall <nervall@hotmail.com>
2019-12-17 17:55:26 +0100

    changed var to let and deleted console.logs

--- app/src/components/game.js
+++ app/src/components/game.js
@@ -3,28 +3,23 @@ import '../App.css';
 
 import Chess from 'chess.js'
 import 'react-chessground/dist/styles/chessground.css'
-var Chessground = require("chessground").Chessground;
+const Chessground = require("chessground").Chessground;
 
 const Game = () => {
   const chess = new Chess()
-  console.log(chess)
 
- useEffect(() => {
-   
-  Chessground(document.body, {});
-  
+ useEffect(() => {  
+  Chessground(document.body, {}); 
  }, [])
 
  while (!chess.game_over()) {
-  var moves = chess.moves();
-  var move = moves[Math.floor(Math.random() * moves.length)];
+  const moves = chess.moves();
+  const move = moves[Math.floor(Math.random() * moves.length)];
   chess.move(move);
 }
-console.log(chess.pgn());
 
 return (
   <div className="App">
-
   </div>
   )
 }
