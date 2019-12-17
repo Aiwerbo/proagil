00ceafb57c2fb186ec80f05b1c54165ec9bb7e8e
Jim Nervall <nervall@hotmail.com>
2019-12-17 20:54:24 +0100

    added mocha and a testfile

--- app/src/components/login.js
+++ app/src/components/login.js
@@ -29,6 +29,7 @@ const Login = () => {
   return (
     <div className='login-container'>
       <div className='login-box'>
+        <h2 className='login-header'>Play Chess</h2>
         <form>
           <input className='login-input' type="text" name="name" placeholder="Name" onChange={gameName} />
           <button className='login-button' type="submit" onClick={startGame}>Start Game</button>
