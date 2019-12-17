26968735cce5abc74aa36147566998a484c70bd0
Jim Nervall <nervall@hotmail.com>
2019-12-17 17:43:33 +0100

    login component almost done missing axios-call

--- app/src/components/game.js
+++ app/src/components/game.js
@@ -1,26 +1,22 @@
 import React, { useEffect } from 'react';
 import '../App.css';
-
+import 'react-chessground/dist/styles/chessground.css';
 import Chess from 'chess.js'
-import 'react-chessground/dist/styles/chessground.css'
-var Chessground = require("chessground").Chessground;
+
+const Chessground = require("chessground").Chessground;
 
 const Game = () => {
   const chess = new Chess()
-  console.log(chess)
 
  useEffect(() => {
-   
   Chessground(document.body, {});
-  
  }, [])
 
  while (!chess.game_over()) {
-  var moves = chess.moves();
-  var move = moves[Math.floor(Math.random() * moves.length)];
+  const moves = chess.moves();
+  const move = moves[Math.floor(Math.random() * moves.length)];
   chess.move(move);
 }
-console.log(chess.pgn());
 
 return (
   <div className="App">
--- app/src/components/login.js
+++ app/src/components/login.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import '../App.css';
 
 const Login = () => {
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
+  const [auth, setAuth] = useState(false);
+  const [error, setError] = useState('');
 
   const gameName = (e) => {
-    console.log(e.target.value)
     setName(e.target.value);
   }
 
+  const startGame = (e) => {
+    e.preventDefault();
+    if (!name || name.length > 15 || name.length < 3) {
+      setError('You username need to be between 2 and 15 character');
+      return;
+    }
+    setAuth(true)
+  }
+
+
+  if (auth) {
+    return (
+      <Redirect to="/lobby" />
+    )
+  }
   return (
     <div className='login-container'>
       <div className='login-box'>
         <form>
           <input className='login-input' type="text" name="name" placeholder="Name" onChange={gameName} />
-          <button className='login-button' type="submit">Start Game </button>
+          <button className='login-button' type="submit" onClick={startGame}>Start Game</button>
         </form>
+        <div style={{ color: 'red', marginTop: '15px'}}>{error}</div>
       </div>
     </div>
   )
