1f96ea96a506cb5fdd8a21393ac772b25f0c61e0
Andreas Iwerbo <andreas@iwerbo.se>
2019-12-17 10:23:55 +0100

    last setup fix

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,24 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import Chess from 'chess.js';
-import 'react-chessground/dist/styles/chessground.css';
+
+
+//import 'react-chessground/dist/styles/chessground.css';
 
 const Chessground = require('chessground').Chessground;
+//var Chess = require('chess.js');
+
 
 function App() {
   const chess = new Chess();
+  
   console.log(chess);
-
+ 
   useEffect(() => {
+    
     Chessground(document.body, {});
+    
+    
   }, []);
 
   return (
