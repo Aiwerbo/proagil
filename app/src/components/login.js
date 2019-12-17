89521d9d7ac7d40cfc707e2c72ad46dd205f3ea2
Jim Nervall <nervall@hotmail.com>
2019-12-17 16:15:29 +0100

    login component with inputfield and button and some css

--- app/src/components/login.js
+++ app/src/components/login.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../App.css';
 
 const Login = () => {
+  const [name, setName] = useState('')
+
+  const gameName = (e) => {
+    console.log(e.target.value)
+    setName(e.target.value);
+  }
 
   return (
-    <div>Login</div>
+    <div className='login-container'>
+      <div className='login-box'>
+        <form>
+          <input className='login-input' type="text" name="name" placeholder="Name" onChange={gameName} />
+          <button className='login-button' type="submit">Start Game </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
