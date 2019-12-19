4b14f2e4363a9e0b7aab2d5142ee5f1265325613
Andreas Iwerbo <andreas@iwerbo.se>
2019-12-19 11:10:44 +0100

    linting done

--- app/src/components/game.js
+++ app/src/components/game.js
@@ -2,40 +2,32 @@ import React, { useEffect } from 'react';
 import '../App.css';
 import io from 'socket.io-client';
 import 'react-chessground/dist/styles/chessground.css';
-import Chess from 'chess.js'
+import Chess from 'chess.js';
 
-const Chessground = require("chessground").Chessground;
-const socket = io('http://localhost:5010')
+const Chessground = require('chessground').Chessground;
+
+const socket = io('http://localhost:5010');
 const Game = () => {
-  const chess = new Chess()
+  const chess = new Chess();
 
- useEffect(() => {  
-  Chessground(document.body, {}); 
- }, [])
+  useEffect(() => {
+    Chessground(document.body, {});
+  }, []);
 
- while (!chess.game_over()) {
-  const moves = chess.moves();
-  const move = moves[Math.floor(Math.random() * moves.length)];
-  chess.move(move);
-}
+  useEffect(() => {
+    socket.on('welcome', (message) => {
+      console.log(message);
+    });
+  }, []);
 
+  socket.emit('message', { data: 'test' }, (err, response) => {
+    console.log(response.data);
+  });
 
-useEffect(() => {
-  
-  socket.on("welcome", (message) => {
-    console.log(message)
-})
-}, []);
-
-socket.emit('message', {data: 'test'}, (err, response) => {
-  console.log(response.data)
-})
-
-
-return (
-  <div className="App">
-  </div>
-  )
-}
+  return (
+    <div className="App">
+    </div>
+  );
+};
 
 export default Game;
--- socketio-server/socketserver.js
+++ socketio-server/socketserver.js
@@ -1,19 +1,14 @@
-const io = require("socket.io");
-const port = 5010
+const io = require('socket.io');
+
+const port = 5010;
 const server = io.listen(port);
 
-
-server.on("connection", (socket) => {
-  console.log("user connected");
-  socket.emit("welcome", "User connected to Chess server");
+server.on('connection', (socket) => {
+  console.log('user connected');
+  socket.emit('welcome', 'User connected to Chess server');
 
   socket.on('message', (msg) => {
-    console.log(msg)
-    socket.emit('welcome',{data: `Hey from server. You sent me: ${msg.data}`})
-  })
-
+    socket.emit('welcome', { data: `Hey from server. You sent me: ${msg.data}` });
+  });
 });
-
-
-
-console.log(`Socket.io server started on port ${port}` )
+console.log(`Socket.io server started on port ${port}`);
