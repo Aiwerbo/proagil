19760c62c21359c0c63176d0054e516df0a20b3a
Andreas Iwerbo <andreas@iwerbo.se>
2020-01-08 18:47:31 +0100

    added tests and a socket io testserver

---
+++ socketio-server/test/socketserver.test.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const should = chai.should();
+const io = require('socket.io-client');
+let client1;
+let client2;
+
+beforeEach((done) => {
+  client1 = io('http://localhost:5020');
+  client2 = io('http://localhost:5020');
+  done();
+});
+afterEach((done) => {
+  client1.disconnect();
+  client2.disconnect();
+  done();
+});
+
+describe("Socket server connection", () => {
+  it('users trying to connected ', (done) => {
+    client1.on('connect', (data) => {
+      done();
+    });
+  });
+});
+
+describe("Server respond", () => {
+  it('User sending chess object ', (done) => {
+    const obj = {room: '123', turnColor: 'black', fen: '456'};
+    client1.emit('message', {data: obj}, () => {
+    });
+    client2.on('123', (message) => {
+      message.data.should.be.an('object');
+      message.data.should.have.all.keys('room', 'turnColor', 'fen');
+      message.data.fen.should.be.a('string');
+      message.data.turnColor.should.be.a('string');
+      done();
+    });
+  });
+});
---
+++ socketio-server/testsocketserver.js
@@ -0,0 +1,17 @@
+const port = 5020;
+const io = require('socket.io').listen(port);
+
+io.sockets.on('connection', (socket) => {
+  console.log('user connected');
+  socket.emit('welcome', 'User connected to Chess server');
+
+  socket.on('message', (msg) => {
+    console.log(msg)
+    socket.broadcast.emit(msg.data.room, { data: msg.data });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+});
+console.log(`Socket.io server started on port ${port}`);
